cc8e5e134dee494995b64a5a56fda9292649a3ef
Aaron Liu <aaron.liu.bc@gmail.com>
2021-02-21 20:52:42 -0800

    add playerlist and player

---
+++ src/Player.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Player extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    function render() {
+
+}
+}
---
+++ src/PlayerList.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import Player from './Player'
+
+class PlayerList extends Component {
+    constructor(props) {
+        super(props);
+    }
+}
