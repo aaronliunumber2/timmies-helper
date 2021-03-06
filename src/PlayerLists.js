00a49581f4456a714ed09dde20f23bb974fd7ce5
Aaron Liu <aaron.liu.bc@gmail.com>
2021-03-06 15:04:02 -0800

    click list header to hide

--- src/PlayerLists.js
+++ src/PlayerLists.js
@@ -4,6 +4,32 @@ import PlayerList from './PlayerList'
 class PlayerLists extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            showList: []
+        };
+
+        this.clickHeader = this.clickHeader.bind(this);
+    }
+
+    componentDidMount() {
+        //initialize the showList array
+        let showList = [];
+        for (let i = 0; i < this.props.playerLists.length; i++) {
+            showList = [...showList, true];
+        }
+
+        this.setState({ showList });
+    }
+
+
+    clickHeader(id) {
+        //shallow copy of entire array
+        let newShowList = [...this.state.showList];
+        let show = newShowList[id - 1];
+        newShowList[id - 1] = !show;
+        console.log(newShowList);
+        this.setState({ showList : newShowList });
     }
 
     render() {
@@ -14,8 +40,10 @@ class PlayerLists extends Component {
                         this.props.playerLists.map((set) => {
                             return(
                                 <li className='app-list' key={set.id} >
-                                    <h2>List {set.id}</h2>
-                                    <PlayerList players={set.players} games={this.props.games} teams={this.props.teams} />
+                                    <h2 className='list-header' onClick={(e) => this.clickHeader(set.id)}><a href="#">List {set.id}</a></h2>
+                                    <div>
+                                        {this.state.showList[set.id-1] && <PlayerList players={set.players} games={this.props.games} teams={this.props.teams} />}
+                                    </div>
                                 </li>
                             )
                         })
