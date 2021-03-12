a56b9aac60dbbf77d59ee220eaae57e54e869a61
Aaron Liu <aaron.liu.bc@gmail.com>
2021-03-11 22:03:18 -0800

    change header to timmies helper

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
               crossorigin="anonymous"
           />
           <header>
-            Timmies Helper App  
+            Timmies Helper  
           </header>
           <div>
               <TimmiesApp/>
