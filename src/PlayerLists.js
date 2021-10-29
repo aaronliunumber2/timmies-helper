c20d3694892631a3a88270d390ec7df62212a074
Aaron Liu <aaron.liu.bc@gmail.com>
2021-10-28 21:55:29 -0700

    added +/- next to list to show it can be expanded and collapsed

--- src/PlayerLists.js
+++ src/PlayerLists.js
@@ -40,7 +40,7 @@ class PlayerLists extends Component {
                         this.props.playerLists.map((set) => {
                             return (
                                 <div className="player-list" key={set.id}>
-                                    <h2 className='list-header' ><a href={"#set" + set.id} onClick={(e) => this.clickHeader(set.id)}>List {set.id}</a></h2>
+                                    <h2 className='list-header' ><a href={"#set" + set.id} onClick={(e) => this.clickHeader(set.id)}>{(this.state.showList[set.id-1]) ? "-" : "+"}List {set.id}</a></h2>
                                     <Collapse in={this.state.showList[set.id - 1]}>
                                         <div className='stats-list' id={"#set" + set.id}>
                                             <PlayerList data={set.players} columns={this.props.playerListColumns}/>
