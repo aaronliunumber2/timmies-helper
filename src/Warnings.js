5d15dade06230c70046922893ef09638671f0422
Aaron Liu <aaron.liu.bc@gmail.com>
2022-01-06 17:48:59 -0800

    add hide warning link

--- src/Warnings.js
+++ src/Warnings.js
@@ -1,34 +1,53 @@
 import React, { Component } from 'react'
 
-export default function Warnings(props) {
+class Warnings extends Component {
+    constructor(props) {
+        super(props);
 
-    const postponedGamesWarning = props.postponedGames.map((game) =>
-        <div key={game.gamePK}>
-            POSTPONED: {game.teams.away.team.name} vs {game.teams.home.team.name}
-        </div>
-    )
+        this.state = {
+            showWarnings: true
+        };
 
-    let playerInjuries;
-    if (props.injuries) {
-        playerInjuries = props.injuries.map((injury) => 
-            <div key={injury.ID}>
-                INJURY: {injury.player} - {injury.status} - {injury.injury}
-            </div>
-        )
+        this.clickHide = this.clickHide.bind(this);
     }
 
-    const otherWarnings = props.otherWarnings.map((warning, i) =>
-        <div key={i}>
-            Warning: {warning}
-        </div>
-    )
+    clickHide() {
+        this.setState({ showWarnings: !this.state.showWarnings });
+    }
 
-    return (
-        <div className="warnings">
-            {postponedGamesWarning}
-            {playerInjuries}
-            {otherWarnings}
-        </div>
+
+    render() {
+        const postponedGamesWarning = this.props.postponedGames.map((game) =>
+            <div key={game.gamePK}>
+                POSTPONED: {game.teams.away.team.name} vs {game.teams.home.team.name}
+            </div>
         )
+
+        let playerInjuries;
+        if (this.props.injuries) {
+            playerInjuries = this.props.injuries.map((injury) =>
+                <div key={injury.ID}>
+                    INJURY: {injury.player} - {injury.status} - {injury.injury}
+                </div>
+            )
+        }
+
+        const otherWarnings = this.props.otherWarnings.map((warning, i) =>
+            <div key={i}>
+                Warning: {warning}
+            </div>
+        )
+
+        const hideShow =
+            <div onClick={(e) => this.clickHide()}><a href="#">{(this.state.showWarnings) ? "(hide warnings)" : "(show warnings)"}</a></div>
+
+        return (<div className="warnings">
+            {this.state.showWarnings && postponedGamesWarning}
+            {this.state.showWarnings && playerInjuries}
+            {this.state.showWarnings && otherWarnings}
+            {(this.props.postponedGames.length > 0) && (this.props.injuries.length > 0) &&(this.props.otherWarnings.length > 0) && hideShow}
+        </div>)
+    }
 }
 
+export default Warnings
